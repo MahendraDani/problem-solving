ce670e9106607c6b36d2a593b21a1ab64b1164fe
Mahendra Dani <danimahendra0904@gmail.com>
2025-02-24 13:47:54 +0530

    list

---
+++ src/linked-list/linked-list.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "linked-list.h"
+
+int add(int a, int b) {
+  return a + b:
+}
---
+++ src/linked-list/linked-list.h
@@ -0,0 +1,7 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+
+int add(int a,int b);
+
+#endif LINKEDLIST_H
