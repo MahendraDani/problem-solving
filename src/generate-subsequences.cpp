0b9655dc43d81936d80e09a482588c4e40eea97f
Mahendra Dani <danimahendra0904@gmail.com>
2025-04-05 23:03:43 +0530

    update

--- src/generate-subsequences.cpp
+++ src/generate-subsequences.cpp
@@ -13,16 +13,31 @@ void solve(int idx,string ip, string op,set<string>& st){
   solve(idx + 1, ip, op,st);
 }
 
+// WRONG
+void solve2(int idx, string ip,string op,vector<int>& hash){
+  if(idx >= ip.size()){
+    cout << op << " ";
+    return;
+  }
+
+  if(hash[ip[idx]-'a'] !=0 ){
+    // if already visited char
+    solve2(idx + 1,ip,op,hash);
+    return;
+  }
+
+  hash[ip[idx]-'a']++;
+  solve2(idx + 1, ip, op + ip[idx],hash);
+  solve2(idx + 1, ip, op,hash);
+}
+
 int main(){
   string ip;
   cin >> ip;
-
+  
   string op = "";
-  set<string> st;
-  solve(0, ip,op,st);
-  for(auto x: st){
-    cout << x << " ";
-  }
+  vector<int> hash(26,0);
+  solve2(0,ip,op,hash);
   cout << "\n";
   return 0;
 }
